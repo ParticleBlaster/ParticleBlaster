79b6d3f7078b8b3cf93d9a6dc77ca0c7d14408ca
Bohan Huang <weepineapple@gmail.com>
2017-03-12 16:15:28 +0800

    Initialised Constants class

---
+++ ParticleBlaster/ParticleBlaster/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ParticleBlaster
+//
+//  Created by Bohan Huang on 12/3/17.
+//  Copyright © 2017 ParticleBlaster. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+}
