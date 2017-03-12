2955304699326d3fc3acd6ef3f825103309cbef5
Bohan Huang <weepineapple@gmail.com>
2017-03-12 21:19:21 +0800

    Updated GameScene to conform to SKPhisicsContactDelegate

--- ParticleBlaster/ParticleBlaster/GameScene.swift
+++ ParticleBlaster/ParticleBlaster/GameScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     private let player = SKSpriteNode(imageNamed: "player")
     
