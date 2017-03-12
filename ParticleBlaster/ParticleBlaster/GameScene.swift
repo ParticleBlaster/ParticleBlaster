33ead468da1500383fb1487070348320f3e1c7fc
Bohan Huang <weepineapple@gmail.com>
2017-03-12 22:25:17 +0800

    Redefined monster actions to trigger game lose

--- ParticleBlaster/ParticleBlaster/GameScene.swift
+++ ParticleBlaster/ParticleBlaster/GameScene.swift
@@ -76,7 +76,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // Create the actions
         let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: actualY), duration: TimeInterval(actualDuration))
         let actionMoveDone = SKAction.removeFromParent()
-        monster.run(SKAction.sequence([actionMove, actionMoveDone]))
+        let loseAction = SKAction.run() {
+            let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+            let gameOverScene = GameOverScene(size: self.size, won: false)
+            self.view?.presentScene(gameOverScene, transition: reveal)
+        }
+        monster.run(SKAction.sequence([actionMove, loseAction, actionMoveDone]))
         
     }
     
