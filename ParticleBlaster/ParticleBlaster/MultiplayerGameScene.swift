3fb784c80d7c055ef541c24910bbe9948abf3f6c
Bohan Huang <weepineapple@gmail.com>
2017-03-23 15:56:57 +0800

    Initialised Multiplayer Game Scene

---
+++ ParticleBlaster/ParticleBlaster/MultiplayerGameScene.swift
@@ -0,0 +1,14 @@
+//
+//  MultiplayerGameScene.swift
+//  ParticleBlaster
+//
+//  Created by Bohan Huang on 23/3/17.
+//  Copyright © 2017 ParticleBlaster. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class MultiplayerGameScene: SKScene, SKPhysicsContactDelegate {
+
+}
