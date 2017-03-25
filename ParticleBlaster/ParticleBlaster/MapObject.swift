00be85acc23ad3cb0231195b016cd57cff868d07
Bohan Huang <weepineapple@gmail.com>
2017-03-26 02:50:08 +0800

    Added physics body for map object

--- ParticleBlaster/ParticleBlaster/MapObject.swift
+++ ParticleBlaster/ParticleBlaster/MapObject.swift
@@ -24,24 +24,35 @@ class MapObject {
         leftPath.move(to: CGPoint(x: originX, y: originY))
         leftPath.addLine(to: CGPoint(x: originX, y: limitY))
         let leftBoundary = SKShapeNode(path: leftPath)
+        leftBoundary.physicsBody = SKPhysicsBody(edgeFrom: CGPoint(x: originX, y: originY), to: CGPoint(x: originX, y: limitY))
         boundaries.append(leftBoundary)
         
         let rightPath = CGMutablePath()
         rightPath.move(to: CGPoint(x: limitX, y: originY))
         rightPath.addLine(to: CGPoint(x: limitX, y: limitY))
         let rightBoundary = SKShapeNode(path: rightPath)
+        rightBoundary.physicsBody = SKPhysicsBody(edgeFrom: CGPoint(x: limitX, y: originY), to: CGPoint(x: limitX, y: limitY))
         boundaries.append(rightBoundary)
         
         let topPath = CGMutablePath()
         topPath.move(to: CGPoint(x: originX, y: originY))
         topPath.addLine(to: CGPoint(x: limitX, y: originY))
         let topBoundary = SKShapeNode(path: topPath)
+        topBoundary.physicsBody = SKPhysicsBody(edgeFrom: CGPoint(x: originX, y: originY), to: CGPoint(x: limitX, y: originY))
         boundaries.append(topBoundary)
         
         let bottomPath = CGMutablePath()
         bottomPath.move(to: CGPoint(x: originX, y: limitY))
         bottomPath.addLine(to: CGPoint(x: limitX, y: limitY))
         let bottomBoundary = SKShapeNode(path: bottomPath)
+        bottomBoundary.physicsBody = SKPhysicsBody(edgeFrom: CGPoint(x: originX, y: limitY), to: CGPoint(x: limitX, y: limitY))
         boundaries.append(bottomBoundary)
+        
+        for boundary in boundaries {
+            boundary.physicsBody?.isDynamic = false
+            boundary.physicsBody?.categoryBitMask = PhysicsCategory.Map
+            boundary.physicsBody?.contactTestBitMask = PhysicsCategory.All
+            boundary.physicsBody?.collisionBitMask = PhysicsCategory.None
+        }
     }
 }
