107963979edd33494627f7c65299ea5f9046a664
Bohan Huang <weepineapple@gmail.com>
2017-03-30 00:15:20 +0800

    Updated GameScene class by extending functionalities

--- ParticleBlaster/ParticleBlaster/GameScene.swift
+++ ParticleBlaster/ParticleBlaster/GameScene.swift
@@ -13,15 +13,48 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var viewController: UIViewController!
     var players: [Player] = [Player]()
     var joystickPlates: [JoystickPlate] = [JoystickPlate]()
-    var joystick: [Joystick] = [Joystick]()
-    var fireButton: [FireButton] = [FireButton]()
+    var joysticks: [Joystick] = [Joystick]()
+    var fireButtons: [FireButton] = [FireButton]()
     
-    var rotateJoystickAndPlayerHandler: [((CGPoint) -> ())] = [((CGPoint) -> ())]()
-    var endJoystickMoveHandler: [(() -> ())] = [(() -> ())]()
+    var rotateJoystickAndPlayerHandlers: [((CGPoint) -> ())] = [((CGPoint) -> ())]()
+    var endJoystickMoveHandlers: [(() -> ())] = [(() -> ())]()
     var playerVelocityUpdateHandlers: [(() -> ())] = [(() -> ())]()
+    var fireHandlers: [(() -> ())] = [(() -> ())]()
     
     var obstacleHitHandler: (() -> ())?
     var obstacleMoveHandler: (() -> ())?
     var obstacleVelocityUpdateHandler: (() -> ())?
-    var fireHandler: (() -> ())?
+    
+    func removeElement(node: SKSpriteNode) {
+        node.removeFromParent()
+    }
+    
+    func displayScoreAnimation(displayScore: Int, scoreSceneCenter: CGPoint) {
+        let score = "+" + String(displayScore)
+        let label = SKLabelNode(fontNamed: Constants.destroyObstacleScoreFont)
+        label.text = score
+        label.fontSize = Constants.destroyObstacleScoreFontSize
+        label.fontColor = SKColor.orange
+        label.position = scoreSceneCenter
+        label.alpha = 0
+        addChild(label)
+        // animation for moving the label upwards by Constants.offset, and change alpha to 1
+        // then moving it upwards also by offset, change alpha to 0; remove label from scene
+        let fadeInAction = SKAction.fadeIn(withDuration: Constants.destroyObstacleScoreFadeTime)
+        let fadeOutAction = SKAction.fadeOut(withDuration: Constants.destroyObstacleScoreFadeTime)
+        let moveAction = SKAction.move(by: Constants.destroyObstacleScoreOffset, duration: Constants.destroyObstacleScoreFadeTime)
+        let labelAction = SKAction.sequence([SKAction.group([fadeInAction, moveAction]), SKAction.group([fadeOutAction, moveAction])])
+        label.run(labelAction, completion: {
+            label.removeFromParent()
+        })
+    }
+    
+    func addSingleObstacle(newObstacle: Obstacle) {
+        newObstacle.shape.position = newObstacle.initialPosition
+        addChild(newObstacle.shape)
+    }
+    
+    func addBoundary(boundary: SKShapeNode) {
+        addChild(boundary)
+    }
 }
