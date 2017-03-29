35c47d690c502b069d5ac2243727144f5b965d89
Bohan Huang <weepineapple@gmail.com>
2017-03-29 17:41:18 +0800

    Implemented GameLogic

--- ParticleBlaster/ParticleBlaster/GameLogic.swift
+++ ParticleBlaster/ParticleBlaster/GameLogic.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import SpriteKit
+
+protocol GameLogic {
+    var gameViewController: GameViewController { get set }
+    var numberOfPlayers: Int { get set }
+    var playerControllers: [PlayerController] { get set }
+    var obstaclePool: [Obstacle] { get set }
+    var map: MapObject { get set }
+    var winningCondition: Bool { get set }
+    var losingCondition: Bool { get set }
+    
+    func updateObstacleVelocityHandler()
+    func bulletObstacleDidCollide(bullet: SKSpriteNode, obstacle: SKSpriteNode)
+    func obstacleDidCollideWithPlayer(obs: SKSpriteNode, player: SKSpriteNode)
+    func obstaclesDidCollideWithEachOther(obs1: SKSpriteNode, obs2: SKSpriteNode)
+    func objectDidCollideWithMap(object: SKSpriteNode)
+}
