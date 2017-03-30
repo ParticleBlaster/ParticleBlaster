c3ab96c18fc581fe3963269700c256ee9adca8fb
Bohan Huang <weepineapple@gmail.com>
2017-03-30 17:14:28 +0800

    Updated MultiplayerViewParams

--- ParticleBlaster/ParticleBlaster/MultiplayerViewParams.swift
+++ ParticleBlaster/ParticleBlaster/MultiplayerViewParams.swift
@@ -22,28 +22,45 @@ class MultiplayerViewParams {
     static var fireButtonWidth: CGFloat!
     static var fireButtonHeight: CGFloat!
     
-    static var playerCenterX: CGFloat!
-    static var playerCenterY: CGFloat!
-    static var joystickPlateCenterX: CGFloat!
-    static var joystickPlateCenterY: CGFloat!
-    static var fireButtonCenterX: CGFloat!
-    static var fireButtonCenterY: CGFloat!
+    static var playerCenterX1: CGFloat!
+    static var playerCenterY1: CGFloat!
+    static var joystickPlateCenterX1: CGFloat!
+    static var joystickPlateCenterY1: CGFloat!
+    static var fireButtonCenterX1: CGFloat!
+    static var fireButtonCenterY1: CGFloat!
+    
+    static var playerCenterX2: CGFloat!
+    static var playerCenterY2: CGFloat!
+    static var joystickPlateCenterX2: CGFloat!
+    static var joystickPlateCenterY2: CGFloat!
+    static var fireButtonCenterX2: CGFloat!
+    static var fireButtonCenterY2: CGFloat!
 
     static func initializeJoystickInfo(viewSize: CGSize) {
-        self.joystickPlateWidth = viewSize.width / 8
-        self.joystickPlateHeight = viewSize.width / 8
-        self.joystickPlateCenterX = viewSize.width * 0.1
-        self.joystickPlateCenterY = viewSize.height * 0.1
+        self.joystickPlateWidth = viewSize.width / 16
+        self.joystickPlateHeight = viewSize.width / 16
         self.joystickWidth = self.joystickPlateWidth / 2
         self.joystickHeight = self.joystickPlateHeight / 2
-        self.fireButtonCenterX = viewSize.width * 0.9
-        self.fireButtonCenterY = viewSize.height * 0.1
         self.fireButtonWidth = self.joystickPlateWidth
         self.fireButtonHeight = self.joystickPlateHeight
         
-        self.playerCenterX = viewSize.width * 0.1
-        self.playerCenterY = viewSize.height * 0.5
+        self.playerCenterX1 = viewSize.width * 0.1
+        self.playerCenterY1 = viewSize.height * 0.5
+        
+        self.joystickPlateCenterX1 = viewSize.width * 0.1
+        self.joystickPlateCenterY1 = viewSize.height * 0.1
+        
+        self.fireButtonCenterX1 = viewSize.width * 0.1
+        self.fireButtonCenterY1 = viewSize.height * 0.9
+       
+        self.playerCenterX2 = viewSize.width * 0.9
+        self.playerCenterY2 = viewSize.height * 0.5
+        
+        self.joystickPlateCenterX2 = viewSize.width * 0.9
+        self.joystickPlateCenterY2 = viewSize.height * 0.9
+        
+        self.fireButtonCenterX2 = viewSize.width * 0.9
+        self.fireButtonCenterY2 = viewSize.height * 0.1
+        
     }
-    
-    
 }
