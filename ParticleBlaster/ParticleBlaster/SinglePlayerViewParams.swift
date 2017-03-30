9a9450163c0d6b7c7fb512286a1b2759ab592b22
Bohan Huang <weepineapple@gmail.com>
2017-03-30 17:12:50 +0800

    Created SinglePlayerViewParams to store view parameters related to Single Player mode

---
+++ ParticleBlaster/ParticleBlaster/SinglePlayerViewParams.swift
@@ -0,0 +1,9 @@
+//
+//  SinglePlayerViewParams.swift
+//  ParticleBlaster
+//
+//  Created by Bohan Huang on 30/3/17.
+//  Copyright © 2017 ParticleBlaster. All rights reserved.
+//
+
+import Foundation
