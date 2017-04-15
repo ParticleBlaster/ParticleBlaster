9938cee1eba0458f4c40234d0ec9106a080f51a2
Bohan Huang <weepineapple@gmail.com>
2017-04-15 21:31:51 +0800

    Changed a initialiser in Obstacle into convenient initialiser

--- ParticleBlaster/ParticleBlaster/Obstacle.swift
+++ ParticleBlaster/ParticleBlaster/Obstacle.swift
@@ -32,20 +32,16 @@ class Obstacle : GameObject {
 
     /* Start of initializors and supporting functions */
     // Initialize with the image name provided
-    init(image: String, userSetInitialPosition: CGPoint) {
-        self.imageName = image
-        self.initialPosition = userSetInitialPosition
-        super.init(imageName: image)
-        self.timeToLive = Constants.defaultTimeToLive
-        setupShape()
-    }
-    
-    init(image: String, userSetInitialPosition: CGPoint, timeToLIve: Int) {
+    init(image: String, userSetInitialPosition: CGPoint, timeToLive: Int) {
         self.imageName = image
         self.initialPosition = userSetInitialPosition
         super.init(imageName: image)
         self.timeToLive = timeToLive
         setupShape()
+    }
+    
+    convenience init(image: String, userSetInitialPosition: CGPoint) {
+        self.init(image: image, userSetInitialPosition: userSetInitialPosition, timeToLive: Constants.defaultTimeToLive)
     }
 
     // Initialize with the same attributes of an existing obstacle, discarding the original physics body
